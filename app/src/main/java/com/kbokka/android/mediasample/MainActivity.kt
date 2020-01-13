bba671c3a1d5de7b06c5354417462f6f36acaf7e
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2020-01-13 12:04:55 +0900

    Add media player to main activity

--- app/src/main/java/com/kbokka/android/mediasample/MainActivity.kt
+++ app/src/main/java/com/kbokka/android/mediasample/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.kbokka.android.mediasample
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import java.io.IOException
+import java.lang.IllegalArgumentException
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  private var _player: MediaPlayer? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    _player = MediaPlayer()
+    val mediaFileUriStr = "android.resource://${packageName}/${R.raw.summer_mountain}"
+    val mediaFileUri = Uri.parse(mediaFileUriStr)
+
+    try {
+      _player?.setDataSource(applicationContext, mediaFileUri)
+      _player?.setOnPreparedListener(PlayPreparedListener())
+      _player?.setOnCompletionListener(PlayCompletionListener())
+      _player?.prepareAsync()
+    } catch (ex: IllegalArgumentException) {
+      Log.e("MediaSample", "メディアプレイヤー準備時の例外発生", ex)
+    } catch (ex: IOException) {
+      Log.e("MediaSample", "メディアプレイヤー準備時の例外発生", ex)
     }
+  }
+
+  private inner class PlayPreparedListener : MediaPlayer.OnPreparedListener {
+    override fun onPrepared(mp: MediaPlayer) {
+      findViewById<Button>(R.id.btPlay).isEnabled = true
+      findViewById<Button>(R.id.btBack).isEnabled = true
+      findViewById<Button>(R.id.btForward).isEnabled = true
+    }
+  }
+
+  private inner class PlayCompletionListener : MediaPlayer.OnCompletionListener {
+    override fun onCompletion(mp: MediaPlayer) {
+      findViewById<Button>(R.id.btPlay).setText(R.string.bt_play_play)
+    }
+  }
 }
